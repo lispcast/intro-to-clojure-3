c30857ed83e229e3cf8b9cfddb715aaedfb36e07
Eric Normand <ericwnormand@gmail.com>
2024-10-24 15:21:05 -0500

    Fetch flour code

--- src/intro_to_clojure/day2.clj
+++ src/intro_to_clojure/day2.clj
@@ -2,3 +2,11 @@
   (:require [bakery.robot :as robot]
             [intro-to-clojure.day1 :as day1]))
 
+(comment
+  (robot/go-to :pantry)
+  (robot/load-up :flour)
+  (robot/go-to :prep-area)
+  (robot/unload :flour)
+
+
+  (robot/status))
