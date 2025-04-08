cdda0e123b3a72dd7e38d7233300a3b7431d01e4
Eric Normand <ericwnormand@gmail.com>
2025-04-07 21:12:19 -0500

    010

---
+++ day1/010 exercise 1.5/solution.clj
@@ -0,0 +1,18 @@
+;; my solution
+
+(defn scooped? [ingredient]
+  (cond
+    (= :flour ingredient)
+    true
+    (= :almond-milk ingredient)
+    true
+    (= :sugar ingredient)
+    true
+    (= :coconut-oil ingredient)
+    true
+    (= :baking-powder ingredient)
+    true
+    (= :corn-starch ingredient)
+    true
+    :else
+    false))
